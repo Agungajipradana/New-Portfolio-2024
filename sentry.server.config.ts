2b740de773457e40a9cbd90772c103c3ed42c9c4
Agung aji pradana <agungajipradana@gmail.com>
2024-08-11 17:27:14 +0700

    Add sentry for Issue trackers

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0adcef1b138fe89ee62f0bf69ae0036d@o4507758031339520.ingest.de.sentry.io/4507758038614096",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
